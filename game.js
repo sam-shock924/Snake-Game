21923ad72a54ce7b8646d4a9f1736c17124652a9
Sam Shock <sam.shock924@gmail.com>
2022-01-26 14:54:16 -0500

    basic canvas

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-let
+let canvas = document.getElementById('snake-game-canvas');
+let canvasContext;
+
+window.onload = () => {
+    console.log('hello');
+    drawCanvas();
+    drawSnake();
+}
+
+function drawCanvas() {
+    console.log('sup');
+    canvasContext = canvas.getContext('2d');
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(100, 100, 20, 20);
+}
