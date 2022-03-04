f898553ab5385fd9ec769cd7326911d1e55742ca
Sam Shock <sam.shock924@gmail.com>
2022-03-04 15:43:12 -0500

    Update game.js

--- game.js
+++ game.js
@@ -8,17 +8,37 @@ window.onload = () => {
     console.log('hello');
     drawCanvas();
     drawSnake();
+    window.addEventListener('keydown', function (e) {
+        switch (e.key) {
+            case 'ArrowUp':
+                console.log('ArrowUp')
+                snakePositionY -= 20;
+                break;
+            case 'ArrowDown':
+                console.log('ArrowDown')
+                snakePositionY += 20;
+                break;
+            case 'ArrowLeft':
+                console.log('ArrowLeft')
+                snakePositionX -= 20;
+                break;
+            case 'ArrowRight':
+                console.log('ArrowRight')
+                snakePositionX += 20;
+                break;
+        }
+    })
 }
 
 function drawCanvas() {
-    console.log('sup');
+    console.log('canvasDraw');
     canvasContext = canvas.getContext('2d');
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawSnake() {
-    console.log('snake')
+    console.log('snakeDraw')
     canvasContext.beginPath();
     canvasContext.fillStyle = 'green';
     canvasContext.fillRect(snakePositionX, snakePositionY, 20, 20);
@@ -30,45 +50,7 @@ function moveSnake() {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    window.addEventListener('keydown', function (e) {
-        switch (e.code) {
-            case 'ArrowUp':
-                console.log('ArrowUp')
-                snakePositionY -= 1;
-                break;
-            case 'ArrowDown':
-                console.log('ArrowDown')
-                snakePositionY += 1;
-                break;
-            case 'ArrowLeft':
-                console.log('ArrowLeft')
-                snakePositionX -= 1;
-                break;
-            case 'ArrowRight':
-                console.log('ArrowRight')
-                snakePositionX += 1;
-                break;
-            }
-        })
-    // snakePositionX += 10;    
-    // snakePositionX += 10;
     drawSnake();
 }
-                
-                
-                
-                
-                
-                
-                
+
 setInterval(moveSnake, 500);
-
-
-/* ----------------
-    so to get the snake to move, we need to set the base snake size in a global variable
-    (see call when I asked this at about 1:02:30)
-
-    e.key...keydown
-
-
----------- */
